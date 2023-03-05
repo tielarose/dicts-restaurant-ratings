6f485fac21fe4e4aaed9ec1653e884607b001e7d
vinitha <vinitha.nec@gmail.com>
2023-03-04 16:24:01 -0800

    split code to small function

--- ratings.py
+++ ratings.py
@@ -2,9 +2,10 @@
 # Reads the ratings in from the file
 # Stores them in a dictionary
 # And finally, spits out the ratings in alphabetical order by restaurant
+import sys
 
-def print_restaurant_ratings(filename):
-    """Restaurant rating lister."""
+def read_restaurant_ratings(filename):
+    """Take a file and return as a dic."""
 
     restaurant_ratings_dict = {}
 
@@ -17,9 +18,23 @@ def print_restaurant_ratings(filename):
         # Assign variables for name and rating
         restaurant_name, rating = line.split(':')[0], line.split(':')[1] 
         restaurant_ratings_dict[restaurant_name] = rating # Add restaurant and rating to dictionary
+    
+    return restaurant_ratings_dict
+
+
+def print_restaurant_rating(restaurant_ratings_dict):
+    """takes in dic and printing the rating in alphabetical order"""
 
     # Print each restaurant and its rating in alphabetical order
+
     for restaurant, rating in sorted(restaurant_ratings_dict.items()):
         print(f'{restaurant} is rated at {rating}.')
 
-print_restaurant_ratings('scores.txt')
+
+def main(filename):
+
+    restaurant_rating_dict =  read_restaurant_ratings(filename)
+    print_restaurant_rating(restaurant_rating_dict)
+
+
+main(sys.argv[1])
